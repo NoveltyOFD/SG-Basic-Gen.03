7aca3cddc99d6f310606cac9ba051ede5e786141
NoveltyOFD <vnovelty.ofd@gmail.com>
2017-03-21 20:57:32 +0700

    Sayid M Ridho F

--- DataLatihan/answer1.py
+++ DataLatihan/answer1.py
@@ -1,23 +1,17 @@
-<<<<<<< HEAD
 data1 = "Data1.txt"
-=======
-data1 = "Data_1.txt"
-data2 = "Data_2.txt"
->>>>>>> 3a5b3855996f95c32312d96cf20520b45a49027c
 
 def readData(data1):
 	x = []
 	with open(data1) as data :
 	    for line in data :
-<<<<<<< HEAD
 	    	x = line.split()
 	return x
 
 
-x = readData(data1)
+teks1 = readData(data1)
 
 b = []
-for i in x :
+for i in teks1 :
 	if i == 'I' :
 		b.append('*')
 	elif i == 'and' or i == 'The' or i=='you':
@@ -27,7 +21,3 @@ for i in x :
 
 asd = ' '.join(b)
 print (asd)
-=======
-		    x = line.split()
-	return x
->>>>>>> 3a5b3855996f95c32312d96cf20520b45a49027c
--- DataLatihan/answer2.py
+++ DataLatihan/answer2.py
@@ -1,35 +1,26 @@
-<<<<<<< HEAD
 data1 = "Data1.txt"
 data2 = "Data2.txt"
-=======
-data1 = "Data_1.txt"
-data2 = "Data_2.txt"
->>>>>>> 3a5b3855996f95c32312d96cf20520b45a49027c
 
 def readData(data1):
 	x = []
 	with open(data1) as data :
 	    for line in data :
 		    x = line.split()
-<<<<<<< HEAD
 	return x
 
 teks1 = readData(data1)
 teks2 = readData(data2)
 
-sama = []
+same = []
 
-for i in teks1 :
-	for j in teks2:
-		if i==j :
+for x in teks1 :
+	for y in teks2:
+		if x==y :
 			counter = 0
-			for x in sama :
-				if x == j :
+			for z in same :
+				if z == y :
 					counter += 1
 			if counter == 0 :
-				sama.append(j)
+				same.append(y)
 
-print (sama)
-=======
-return x
->>>>>>> 3a5b3855996f95c32312d96cf20520b45a49027c
+print (same)
